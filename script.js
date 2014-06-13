e2dd2f08e38638d1325d9bdc5b4791d323387d6d
soham1 <sohamashodiya@gmail.com>
2014-06-13 10:05:51 -0700

    New JavaScript File

---
+++ script.js
@@ -0,0 +1,16 @@
+var ageasstring = prompt("What is your age? ", "");
+
+var age = Number(ageasstring);
+
+if (age > 12) { 
+
+alert("You are older than me!");
+
+}
+
+else {
+    alert("You are younger than me");
+}
+
+alert("Thank you for your time.");
+
